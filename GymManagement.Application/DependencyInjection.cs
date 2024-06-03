85e93582bbadef35071e6e32e25d894d29e5718c
Islam Mohamed <eslamzakaria293@gmail.com>
2024-06-03 21:26:03 +0300

    added DependencyInjection config file

--- GymManagement.Application/DependencyInjection.cs
+++ GymManagement.Application/DependencyInjection.cs
@@ -1,7 +1,12 @@
-﻿namespace GymManagement.Application
-{
-    public class DependencyInjection
-    {
+﻿using Microsoft.Extensions.DependencyInjection;
 
+namespace GymManagement.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            return services;
+        }
     }
 }
