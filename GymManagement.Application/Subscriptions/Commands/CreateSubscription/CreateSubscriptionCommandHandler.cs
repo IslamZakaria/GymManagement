be42cf30b25c6dcbb28d9d7c8e9a5c372673b8f2
Islam Mohamed <eslamzakaria293@gmail.com>
2024-08-17 09:52:38 +0300

    refactor: inject repo and UoW

--- GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
+++ GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommandHandler.cs
@@ -1,13 +1,30 @@
 ﻿using ErrorOr;
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Subscriptions;
 using MediatR;
 
 namespace GymManagement.Application.Subscriptions.Commands.CreateSubscription
 {
-    internal sealed class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Guid>>
+    internal sealed class CreateSubscriptionCommandHandler : IRequestHandler<CreateSubscriptionCommand, ErrorOr<Subscription>>
     {
-        public async Task<ErrorOr<Guid>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+        private readonly ISubscriptionRepository _subscriptionRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public CreateSubscriptionCommandHandler(ISubscriptionRepository subscriptionRepository, IUnitOfWork unitOfWork)
         {
-            return Guid.NewGuid();
+            _subscriptionRepository = subscriptionRepository;
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ErrorOr<Subscription>> Handle(CreateSubscriptionCommand request, CancellationToken cancellationToken)
+        {
+            var subscription = new Subscription
+            {
+                Id = Guid.NewGuid()
+            };
+
+            await _subscriptionRepository.AddSubscriptionAsync(subscription);
+            await _unitOfWork.CommitChabgesAsync();
+            return subscription;
         }
     }
 }
