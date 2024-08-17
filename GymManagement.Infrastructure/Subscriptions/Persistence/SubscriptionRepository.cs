f49a7d6cfea5b83183f4a0a4b7b8a85c24ce330a
Islam Mohamed <eslamzakaria293@gmail.com>
2024-08-17 17:07:14 +0300

    feat: implement subscription repo

---
+++ GymManagement.Infrastructure/Subscriptions/Persistence/SubscriptionRepository.cs
@@ -0,0 +1,26 @@
+﻿using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Subscriptions;
+using GymManagement.Infrastructure.Common.Persistence;
+
+namespace GymManagement.Infrastructure.Subscriptions.Persistence
+{
+    internal sealed class SubscriptionRepository : ISubscriptionRepository
+    {
+        private readonly GymManagementDbContext _dbContext;
+        public SubscriptionRepository(GymManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task AddSubscriptionAsync(Subscription subscription)
+        {
+            await _dbContext.subscriptions.AddAsync(subscription);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public async Task<Subscription?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.subscriptions.FindAsync(id);
+        }
+    }
+}
