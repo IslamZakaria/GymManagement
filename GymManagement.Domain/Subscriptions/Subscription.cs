4fc718ec3e7b872e33eb92d83aa6f247ca9c8d7d
Islam Mohamed <eslamzakaria293@gmail.com>
2024-09-01 13:08:56 +0300

    refactor: update subscription entity

--- GymManagement.Domain/Subscriptions/Subscription.cs
+++ GymManagement.Domain/Subscriptions/Subscription.cs
@@ -2,7 +2,22 @@
 {
     public class Subscription
     {
-        public Guid Id { get; set; }
-        public string SubscriptionType { get; set; } = string.Empty;
+        public Guid Id { get; private set; }
+        public Guid _adminId;
+        public SubscriptionType SubscriptionType { get; private set; }
+
+        public Subscription(SubscriptionType subscriptionType,
+                            Guid adminId,
+                            Guid? id)
+        {
+            SubscriptionType = subscriptionType;
+            _adminId = adminId;
+            Id = id ?? Guid.NewGuid();
+        }
+
+        private Subscription()
+        {
+
+        }
     }
 }
