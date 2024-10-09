493b582b72bca69d7a9994bcb59fee734df11434
Islam Mohamed <eslamzakaria293@gmail.com>
2024-10-09 21:02:16 +0300

    feat: implement get gym

---
+++ GymManagement.Application/Gyms/Queries/Get/GetGymQuery.cs
@@ -0,0 +1,8 @@
+﻿using ErrorOr;
+using GymManagement.Domain.Gyms;
+using MediatR;
+
+namespace GymManagement.Application.Gyms.Queries.Get
+{
+    public record GetGymQuery(Guid SubscriptionId, Guid GymId) : IRequest<ErrorOr<Gym>>;
+}
