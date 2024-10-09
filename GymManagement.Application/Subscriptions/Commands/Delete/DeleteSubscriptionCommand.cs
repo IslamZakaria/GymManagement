e82858a81f5e6553f484a6d069a547762c8d140e
Islam Mohamed <eslamzakaria293@gmail.com>
2024-10-09 16:41:36 +0300

    feat: add delete subscription

---
+++ GymManagement.Application/Subscriptions/Commands/Delete/DeleteSubscriptionCommand.cs
@@ -0,0 +1,7 @@
+﻿using ErrorOr;
+using MediatR;
+
+namespace GymManagement.Application.Subscriptions.Commands.Delete
+{
+    public record DeleteSubscriptionCommand(Guid Id) : IRequest<ErrorOr<Deleted>>;
+}
