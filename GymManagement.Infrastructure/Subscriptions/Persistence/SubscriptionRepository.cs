c79b3e19ef73629fe20877521b095474f3023bb4
Islam Mohamed <eslamzakaria293@gmail.com>
2024-10-09 15:29:14 +0300

    refactor: removed useless line

--- GymManagement.Infrastructure/Subscriptions/Persistence/SubscriptionRepository.cs
+++ GymManagement.Infrastructure/Subscriptions/Persistence/SubscriptionRepository.cs
@@ -16,7 +16,6 @@ namespace GymManagement.Infrastructure.Subscriptions.Persistence
         public async Task AddSubscriptionAsync(Subscription subscription)
         {
             await _dbContext.Subscriptions.AddAsync(subscription);
-            await _dbContext.SaveChangesAsync();
         }
 
         public async Task<bool> ExistsAsync(Guid id)
