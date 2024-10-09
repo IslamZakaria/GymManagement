82579ea673af43f2b4488647231c322062829142
Islam Mohamed <eslamzakaria293@gmail.com>
2024-10-09 15:28:48 +0300

    feat: implement gym repo

---
+++ GymManagement.Application/Common/Interfaces/IGymsRepository.cs
@@ -0,0 +1,15 @@
+﻿using GymManagement.Domain.Gyms;
+
+namespace GymManagement.Application.Common.Interfaces
+{
+    public interface IGymsRepository
+    {
+        Task AddAsync(Gym gym);
+        Task<Gym?> GetByIdAsync(Guid id);
+        Task<bool> ExistsAsync(Guid id);
+        Task<List<Gym>> ListBySubscriptionIdAsync(Guid subscriptionId);
+        Task Updatesync(Gym gym);
+        Task RemoveAsync(Gym gym);
+        Task RemoveRangeAsync(List<Gym> gyms);
+    }
+}
