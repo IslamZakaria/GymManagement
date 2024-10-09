cac0d97793fc231ff254b34735d385d5dc3516b4
Islam Mohamed <eslamzakaria293@gmail.com>
2024-10-09 17:06:28 +0300

    feat: implement create room

---
+++ GymManagement.Application/Rooms/Commands/Create/CreateRoomCommand.cs
@@ -0,0 +1,10 @@
+﻿using ErrorOr;
+using GymManagement.Domain.Rooms;
+using MediatR;
+
+namespace GymManagement.Application.Rooms.Commands.Create
+{
+    public record CreateRoomCommand(
+        Guid GymId,
+        string RoomName) : IRequest<ErrorOr<Room>>;
+}
