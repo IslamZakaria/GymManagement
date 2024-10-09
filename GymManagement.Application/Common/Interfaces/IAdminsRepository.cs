1e81e80ea3049c386a27b02fb0c98022e0fe4315
Islam Mohamed <eslamzakaria293@gmail.com>
2024-10-09 15:20:13 +0300

    feat: implement admin repo

---
+++ GymManagement.Application/Common/Interfaces/IAdminsRepository.cs
@@ -0,0 +1,10 @@
+﻿using GymManagement.Domain.Admins;
+
+namespace GymManagement.Application.Common.Interfaces
+{
+    public interface IAdminsRepository
+    {
+        Task<Admin?> GetByIdAsync(Guid id);
+        Task UpdateAsync(Admin admin);
+    }
+}
---
+++ GymManagement.Infrastructure/Admins/Persistence/AdminsRepository.cs
@@ -0,0 +1,30 @@
+﻿using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Admins;
+using GymManagement.Infrastructure.Common.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagement.Infrastructure.Admins.Persistence
+{
+    internal sealed class AdminsRepository : IAdminsRepository
+    {
+        private readonly GymManagementDbContext _dbContext;
+        public AdminsRepository(GymManagementDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Admin?> GetByIdAsync(Guid id)
+        {
+            return await _dbContext.Admins
+                .AsNoTracking()
+                .FirstOrDefaultAsync(admin => admin.Id == id);
+        }
+
+        public Task UpdateAsync(Admin admin)
+        {
+            _dbContext.Update(admin);
+
+            return Task.CompletedTask;
+        }
+    }
+}
